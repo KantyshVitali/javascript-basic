7530d027a2a1ec58f429fa5427cb0551a8d5b79d
KantyshVitali <kantyshvitali@gmail.com>
2020-07-09 12:38:28 +0300

    task4

---
+++ task4/ECMAScript/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+let officialNameOfJs = prompt("What's the official name of JavaScript?",'');
+
+if(officialNameOfJs === "ECMAScript"){
+    alert("Правильно!");
+}
+else{
+    alert("You don't know? “ECMAScript”!");
+}
