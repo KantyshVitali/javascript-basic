599e61341fd1f0ec12510ed180c5af4727142b3d
KantyshVitali <kantyshvitali@gmail.com>
2020-07-14 22:23:05 +0300

    shuffle

---
+++ task17/shuffle/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let shuffle = function(arr) {
+    for(let i = arr.length - 1; i > 0; i-- ) {
+        let j = Math.floor(Math.random() * (i + 1));
+        alert('i' + i);
+        alert('j' + j);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+let max = 7;
+let rand = Math.random() * (max + 1);
+
+alert( Math.floor(rand) );
+
+let count = {
+    '123': 0,
+    '132': 0,
+    '213': 0,
+    '231': 0,
+    '321': 0,
+    '312': 0
+};
+  
+for (let i = 0; i < 1000000; i++) {
+    let arr = [1, 2, 3];
+    shuffle(arr);
+    count[arr.join('')]++;
+}
+  
+  // показать количество всех возможных вариантов
+for (let key in count) {
+    alert(`${key}: ${count[key]}`);
+}
