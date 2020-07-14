aa810981ffc6c93b4e8db628d8bacad247803f65
KantyshVitali <kantyshvitali@gmail.com>
2020-07-14 22:28:48 +0300

    shuffle

--- task17/shuffle/script.js
+++ task17/shuffle/script.js
@@ -2,9 +2,7 @@
 
 let shuffle = function(arr) {
     for(let i = arr.length - 1; i > 0; i-- ) {
-        let j = Math.floor(Math.random() * (i + 1));
-        alert('i' + i);
-        alert('j' + j);
+        let j = Math.floor(Math.random() * (i + 1));        
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 }
