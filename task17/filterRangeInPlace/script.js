9e44d19a798aa4ff9acde55587b8be231ec9820b
KantyshVitali <kantyshvitali@gmail.com>
2020-07-14 12:06:05 +0300

    filterRangeInPlace

---
+++ task17/filterRangeInPlace/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let filterRangeInPlace = function(arr, start, end) {
+    
+    for(let i = 0; i < arr.length; i++) {
+        if( arr[i] < start || arr[i] > end ) {
+            arr.splice(i, 1);
+            i--;    
+        }
+    }    
+};
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+alert( arr ); // [3, 1]
