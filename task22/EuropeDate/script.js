515bd04075600bad54b6696e2bf9460f79f0d42f
KantyshVitali <kantyshvitali@gmail.com>
2020-07-15 17:01:14 +0300

    European date format
    convert standart getDay() format to european format

---
+++ task22/EuropeDate/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let getLocalDay = (date) => (date.getDay() == 0) ?  7 :  date.getDay();
+
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getLocalDay(date) );       // вторник, нужно показать 2
+
